b13ac948c2a913d56b78ebaf9af1ea6ec22d22d3
Joanilza <jbockhorny@gmail.com>
2020-02-03 13:03:04 -0300

    deploy

--- api/controllers/productscontrollers.js
+++ api/controllers/productscontrollers.js
@@ -104,31 +104,4 @@ class ProductController {
   }
 }
 
-// import models from '../server/src/models'
-
-// const getAllProducts = async (req, res) => {
-
-//   const products = await models.Product.findAll({ raw: true })
-//   res.send(products)
-// };
-
-// const postProduct = async (req, res) => {
-
-//   const newProduct = await models.Product.create(req.body)
-//   res.send(newProduct)  
-// };
-
-// const getProductId = async (req, res) => {
-
-//   const productId = await models.Product.findByPk(Number(req.params.id)
-//   )
-//   res.send(productId)  
-// };
-
-// export default {
-//   getAllProducts,
-//   postProduct,
-//   getProductId
-// }
-
 export default ProductController
